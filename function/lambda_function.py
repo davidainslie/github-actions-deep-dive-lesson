109eaea4713766bc1aa807691523f815a933e296
David Ainslie <dainslie@gmail.com>
2022-04-17 19:49:09 +0100

    New workflow

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -1,19 +1,17 @@
 from github import Github
 
 def lambda_handler(event, context):
-    """Lambda function wrapper
-    Args:
-        event: trigger event dict
-        context: lambda methods and properties
-    Returns:
-        string: greeting response
-    """
-    print('Starting functions\n---------------------------------------------'
+  """
+  Lambda function wrapper
+  Args:
+      event: trigger event dict
+      context: lambda methods and properties
+  Returns:
+      string: greeting response
+  """
+  print('Starting functions\n---------------------------------------------')
 
-    if event["input"] == "Hello":
-
-        return "World"
-
-    else:
-
-        raise
+  if event["input"] == "Hello":
+    return "World"
+  else:
+    raise
