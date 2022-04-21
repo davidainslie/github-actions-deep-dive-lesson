8a08f73043e36ba4ddbd90be27f4df602dbe2baf
David Ainslie <dainslie@gmail.com>
2022-04-21 20:58:23 +0100

    Test lambda

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "access-key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret-key" {
+  type      = string
+  sensitive = true
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket" {
+  type = string
+}
